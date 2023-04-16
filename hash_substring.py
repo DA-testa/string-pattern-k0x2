4ef771e22016e82c13d5e2e9736ae6bedd0d0d76
k0x2 <125827996+k0x2@users.noreply.github.com>
2023-04-16 14:31:14 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -3,7 +3,17 @@
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    choice = input()
+    if "I" in choice:
+        raksts = input().rstrip()
+        teksts = input().rstrip()
+    elif "F" in choice:
+        nose = "06"
+        if "a" not in nose:
+            with open(f"tests/{nose}") as file:
+                text = file.read().split("\n")
+                raksts = text[0].rstrip()
+                teksts = text[1].rstrip()
     
     # after input type choice
     # read two lines 
@@ -13,7 +23,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (raksts, teksts)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,9 +31,16 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
+    ocx = []
+    r = len(raksts)
+    t = len(teksts)
+    for i in range(t-r+1):
+        text = teksts[i:i+r]
+        if hash(raksts) == hash(text):
+            ocx.append(i)
+    return ocx
     # and return an iterable variable
-    return [0]
+    
 
 
 # this part launches the functions

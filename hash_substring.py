7c1c3af8e2cdc22e7262e37666b56b62a2492ef4
k0x2 <125827996+k0x2@users.noreply.github.com>
2023-04-16 14:39:29 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,3 +1,4 @@
+# Karlis Olmanis 221RDB255
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow

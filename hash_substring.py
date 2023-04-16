1de95ec139362d8e7f99854927464a9a81b086de
k0x2 <125827996+k0x2@users.noreply.github.com>
2023-04-16 14:35:41 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,20 +1,18 @@
-# python3
-
 def read_input():
     # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    choice = input()
-    if "I" in choice:
+    izvele = input()
+    if "I" in izvele:
         raksts = input().rstrip()
         teksts = input().rstrip()
-    elif "F" in choice:
-        nose = "06"
-        if "a" not in nose:
-            with open(f"tests/{nose}") as file:
-                text = file.read().split("\n")
-                raksts = text[0].rstrip()
-                teksts = text[1].rstrip()
-    
+    elif "F" in izvele:
+        nos = "06"
+        if "a" not in nos:
+            with open(f"tests/{nos}") as file:
+                txt = file.read().split("\n")
+                raksts = txt[0].rstrip()
+                teksts = txt[1].rstrip()
+                
     # after input type choice
     # read two lines 
     # first line is pattern 
@@ -29,21 +27,19 @@ def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
+def get_occurrences(raksts, teksts):
     # this function should find the occurances using Rabin Karp alghoritm 
-    ocx = []
+    occ = []
     r = len(raksts)
     t = len(teksts)
     for i in range(t-r+1):
-        text = teksts[i:i+r]
-        if hash(raksts) == hash(text):
-            ocx.append(i)
-    return ocx
-    # and return an iterable variable
-    
+        txt = teksts[i:i+r]
+        if hash(raksts) == hash(txt):
+            occ.append(i)
+    return occ
+# return an iterable variable
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
